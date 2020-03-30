d294fa790daaf9e57da9befbcf076b03d6bd5096
r-smolonskij <ruslans.smolonskis@gmail.com>
2020-03-30 14:23:28 +0300

    updated shop data and updated card utils to group items

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
 import "./collection-preview.styles.scss";
 
@@ -8,9 +8,11 @@ const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.filter((item, index) => index < 4).map(({id, ...otherItemProps}) => (
-        <CollectionItem key={id} {...otherItemProps}/>
-      ))}
+      {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
