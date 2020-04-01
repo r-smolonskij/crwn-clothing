32031f10a935172608ab598479f5806ed39ecb08
r-smolonskij <ruslans.smolonskis@gmail.com>
2020-04-01 16:53:16 +0300

    updated stripe integration and updating favicon and title

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 import {
   selectCartItems,
   selectCartTotal
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
         <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payment
+      <br/>
+      4242 4242 4242 4242 - : 01/21 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total}></StripeCheckoutButton>
   </div>
 );
 
